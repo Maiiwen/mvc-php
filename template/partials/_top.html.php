fb08a301c31ab20472f0bb9fb02c976e4e4f8b6e
Maiiwen <maiwen.cat@gmail.com>
2022-04-21 13:56:25 +0200

    adding sc-editor cdn

--- template/partials/_top.html.php
+++ template/partials/_top.html.php
@@ -6,6 +6,8 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
+    <!-- sc-editor -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/sceditor@3/minified/themes/default.min.css" />
     <title><?= $title ?? 'MVC-BLOG' ?></title>
 </head>
