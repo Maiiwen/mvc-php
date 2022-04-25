e5c2cbf13881d4d531c31a25e65373b6049f67a5
Maiiwen <maiwen.cat@gmail.com>
2022-04-25 14:47:37 +0200

    switching to namespaces / autoload, adding twig

--- public/index.php
+++ public/index.php
@@ -1,5 +1,28 @@
 <?php
+
+namespace Index;
+
 date_default_timezone_set('Europe/Paris');
+require_once '../vendor/autoload.php';
+require_once '../core/functions.php';
+
+
+
+spl_autoload_register(function ($classname) {
+    $classname = str_replace('\\', '/', $classname);
+    // var_dump($classname);
+    $namespace = explode('/', $classname);
+    // var_dump($namespace);
+    switch ($namespace[0]) {
+        case 'App':
+            $path = '../src/' . strtolower($namespace[1]) . '/' . $namespace[2] . '.php';
+            break;
+        case 'Core':
+            $path = '../core/' . $namespace[1] . '.php';
+            break;
+    }
+    require_once $path;
+});
 
 if (!empty($_GET['controller'])) {
     $controller = ucfirst($_GET['controller']);
@@ -16,8 +39,8 @@ if (file_exists('../src/controller/' . $controller . 'Controller.php')) {
         $action = 'index';
     }
 
-    if (method_exists(($controller . "Controller"), $action)) {
-        ($controller . "Controller")::$action();
+    if (method_exists(('App\\Controller\\' . $controller . "Controller"), $action)) {
+        ('App\\Controller\\' . $controller . "Controller")::$action();
     } else {
         header("HTTP/1.1 404 Not Found");
         echo "Erreur 404 Not Found";
