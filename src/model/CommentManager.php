023938cd2bad4d38c24bb62fe83f4b3533567fc2
Maiiwen <maiwen.cat@gmail.com>
2022-04-27 15:56:39 +0200

    use of parent method getClassName

--- src/model/CommentManager.php
+++ src/model/CommentManager.php
@@ -10,12 +10,10 @@ class CommentManager extends Manager
     public static function getAllByArticleId(int $id)
     {
         $pdo = parent::getPdoInstance();
-        $className = str_replace('Manager', '', get_called_class());
-
+        $className = parent::getClassName();
         $sql = "SELECT * FROM " . lcfirst($className) . " WHERE article_id = :id ORDER BY created_at DESC";
         $query = $pdo->prepare($sql);
         $query->execute(compact('id'));
-
-        return self::hydrateCollection($query->fetchAll(), Comment::class);
+        return self::hydrateCollection($query->fetchAll(), $className);
     }
 }
