e2d019397e19242cdac9f7d4e71ed06cf669dfa9
Maiiwen <maiwen.cat@gmail.com>
2022-05-02 10:53:01 +0200

    change render

--- src/controller/HomeController.php
+++ src/controller/HomeController.php
@@ -9,7 +9,6 @@ class HomeController extends Controller
 {
     public static function index()
     {
-        $indexView = new View('home', 'index');
-        $indexView->render();
+        (new View('home', 'index'))->render();
     }
 }
