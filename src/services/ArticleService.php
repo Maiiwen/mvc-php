52a08010b5eff18861c7d486931d7f90cebbf743
Maiiwen <maiwen.cat@gmail.com>
2022-05-02 10:54:11 +0200

    remove fom generation from services

--- src/services/ArticleService.php
+++ src/services/ArticleService.php
@@ -4,23 +4,6 @@ namespace App\Services;
 
 use Core\Service;
 
-use App\DTO\ArticleDTO;
-use App\Form\Form;
-use Core\FormType\TextareaType;
-use Core\FormType\TextType;
-use Core\FormType\SubmitType;
-use Core\FormType\HiddenType;
-
 class ArticleService extends Service
 {
-    public static function getForm(?ArticleDTO $article = null)
-    {
-        $form = new Form($article ? '/blog/update/' . $article->getId() : '/blog/new', [
-            new TextType('title', $article ? $article->getTitle() : '', 'Titre'),
-            new TextAreaType('content', $article ? $article->getRawContent() : '', 'Contenu', 'textarea'),
-            new HiddenType('id', $article ? $article->getId() : 'new'),
-            new SubmitType('submit', 'submit', 'Ajouter')
-        ]);
-        return $form;
-    }
 }
--- src/services/CommentService.php
+++ src/services/CommentService.php
@@ -2,13 +2,8 @@
 
 namespace App\Services;
 
-use App\DTO\CommentDTO;
 use Core\Service;
-use App\Form\Form;
-use Core\FormType\TextareaType;
-use Core\FormType\TextType;
-use Core\FormType\SubmitType;
-use Core\FormType\HiddenType;
+
 
 class CommentService extends Service
 {
@@ -22,19 +17,5 @@ class CommentService extends Service
             $commentsDTO[] = new $dto($comment);
         }
         return $commentsDTO;
-    }
-
-    public static function getForm(int $articleId, ?CommentDTO $comment = null)
-    {
-        $form = new Form(
-            $comment ? '/blog/comment/edit' . $comment->getId() : '/blog/comment/',
-            [
-                new TextType('author', $comment ? $comment->getAuthor() : '', 'Votre nom'),
-                new TextType('content', $comment ? $comment->getContent() : '', 'Votre commentaire'),
-                new HiddenType('article_id', $articleId),
-                new SubmitType('submit', 'submit', 'Ajouter')
-            ]
-        );
-        return $form;
     }
 }
